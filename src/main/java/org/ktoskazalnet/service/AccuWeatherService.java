44996064f8e8b1ed351fdb62f960c1966866597f
ktoskazalnet <ihoriswiftkey@yandex.ru>
2022-11-29 12:35:49 +0300

    commit

--- src/main/java/org/ktoskazalnet/service/AccuWeatherService.java
+++ src/main/java/org/ktoskazalnet/service/AccuWeatherService.java
@@ -1,7 +1,6 @@
 package org.ktoskazalnet.service;
 
 import org.ktoskazalnet.accuweather_api.current_conditions.Root;
-import org.ktoskazalnet.accuweather_api.exception.CityNotFoundException;
 import org.ktoskazalnet.accuweather_api.locations.City;
 import org.ktoskazalnet.accuweather_api.locations.TopCityCount;
 import org.ktoskazalnet.client.AccuWeatherClient;
